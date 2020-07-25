9219876a32cfe981696d8dd4d46c81ecdd3b0d21
kyoolbaba <kyoolbaba@gmail.com>
2020-07-25 21:03:07 +0530

    Added styles for copy button on color box in pallete

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react'
 import './ColorBox.css'
 export default class ColorBox extends Component {
     render() {
+        const{name ,background}=this.props
         return (
-            <div  style={{background: this.props.background}}  className="ColorBox">
-                <span>
-               {this.props.name}
-                </span>
-                <span>MORE</span>
+            <div  style={{background}}  className="ColorBox">
+             <div className='copycontainer' >
+             <div className='box-content'>
+             <span>{name}</span>
+             </div>
+             <button className='copy-button' >COPY</button>
+             </div>
+             <span>MORE</span>
             </div>
         )
     }
