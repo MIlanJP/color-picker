2d1ef800745dee097bf06850b0a76eef7d5192c6
kyoolbaba <kyoolbaba@gmail.com>
2020-07-25 20:19:02 +0530

    Added color pallete project and created pallete.js and copied seedcolor.js file

---
+++ src/Pallete.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Pallette extends Component {
+    render(){
+        return(
+            <div className="Pallete">
+                <div>
+                
+                </div>
+            </div>
+        )
+    }
+}
